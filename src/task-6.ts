565c71cba03db615a3fddad6d3719a22def078c7
Samsa <motornijsemen@gmail.com>
2026-04-01 19:40:46 +0300

    5

---
+++ src/task-6.ts
@@ -0,0 +1,9 @@
+function getFirstElement<T>(arr: T[]): T {
+  return arr[0];
+}
+
+const num = getFirstElement<number>([1, 2, 3]);          
+const str = getFirstElement<string>(["a", "b", "c"]);   
+const bool = getFirstElement<boolean>([true, false]);   
+
+console.log(num, str, bool);
